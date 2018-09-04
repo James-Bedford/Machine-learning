23f528a255dd7fb21421222da1eb7da910cd03d2
James Bedford <jamesbedford01@hotmail.com>
2018-09-04 11:13:04 +0100

    First lesson

--- lesson1.py
+++ lesson1.py
@@ -1,5 +1,12 @@
 " lesson 1 "
-"apple or an orange "
+"apple or an orange supervised learning "
 
-import sklearn
-import numpy
+from sklearn import tree
+
+
+features = [[140],[130],[150],[170],[200]]
+"labels = [0,0,1,1]"
+labels = ["apple","apple","orange","orange","water melon"]
+clf = tree.DecisionTreeClassifier()
+clf = clf.fit(features,labels)
+print (clf.predict([[185.1]]))
